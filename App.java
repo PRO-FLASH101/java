18a6070f9b5c463b0004e4d47b1cf3dd70532574
PRO-FLASH101 <jnavaneetharaja@gmail.com>
2024-08-05 10:27:14 +0530

    inital commit

---
+++ App.java
@@ -0,0 +1,8 @@
+// ALWAYS USE A CLASS for Java anyways
+//This is my first program
+public class App {
+    public static void main(String[] args) {
+      System.out.println("Hello World");
+      System.out.print("The product of 15*20 is " + 15*20 );
+    }
+  }
