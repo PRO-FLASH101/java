63ab94ce5da32bb4455e84f3282dc09c0b208834
PRO-FLASH101 <jnavaneetharaja@gmail.com>
2024-09-06 18:23:09 -0400

    commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int myfavnum = 7;
+        int userfavnum;
+       
+        // Taking user input
+        Scanner favnum = new Scanner(System.in);
+        System.out.println("Enter your favorite number: ");
+        userfavnum = favnum.nextInt();
+
+        // Comparing favorite numbers
+        if (myfavnum == userfavnum) {
+            System.out.println("Your favorite number is my favorite number🥳!!!");
+
+
+        } else if (myfavnum < userfavnum) {
+            System.out.println("Your favorite number is greater than my favorite number 7. 🥹");
+        } else {
+            System.out.println("Your favorite number is less than my favorite number 7. 😋");
+        }
+    }
+}
